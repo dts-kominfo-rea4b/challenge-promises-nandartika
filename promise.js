64a3048c91c9275079277713c6c90dcd1ef05333
nandartika <kartikanandap@gmail.com>
2022-09-18 22:17:52 +0700

    completed promiseOutput function

--- promise.js
+++ promise.js
@@ -1,7 +1,13 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (keyword) => {
+  let data = await promiseTheaterIXX();
+  data.push(...(await promiseTheaterVGC()));
+
+  const filteredData = data.filter((it) => it.hasil == keyword);
+  return filteredData.length;
+};
 
 module.exports = {
   promiseOutput,
